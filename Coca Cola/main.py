54486e8a8ec0a02919f37163895959c238b7a072
Sameer <kalantrilonda@gmail.com>
2024-04-06 01:59:37 +0530

    Finally Updated

--- Coca Cola/main.py
+++ Coca Cola/main.py
@@ -1,14 +1,12 @@
-def cocacola(n):
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("CocaCola")
-        elif i % 3 == 0:
+def cococola(n):
+    for i in range(1,n+1):
+        if i%3 ==0 and i%5==0 :
+            print("COCOCOLA")
+        elif i%3 ==0:
             print("Coca")
-        elif i % 5 == 0:
+        elif i%5 ==0:
             print("Cola")
         else:
             print(i)
 
-
-n = 20
-cocacola(n)
+print(cococola(15))
