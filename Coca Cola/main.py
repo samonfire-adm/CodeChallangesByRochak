c096d9f19c4132ca2318392edec6d778743ad208
Sameer <kalantrilonda@gmail.com>
2024-04-09 00:01:49 +0530

    minor bug fix

--- Coca Cola/main.py
+++ Coca Cola/main.py
@@ -1,12 +1,12 @@
-def cococola(n):
-    for i in range(1,n+1):
-        if i %3 ==0 and i%5 ==0:
-            print("CocoCola")
-        elif i %3 ==0:
+def cocacola(n):
+    for i in range(1, n+1):
+        if i%3 ==0 and i%5==0:
+            print("Cocacola")
+        elif i%3 ==0:
             print("Coca")
-        elif i %5 ==0:
+        elif i%5 ==0:
             print("Cola")
         else:
             print(i)
 
-print(cococola(15))
+cocacola(15)
